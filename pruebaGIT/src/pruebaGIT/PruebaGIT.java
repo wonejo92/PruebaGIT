0b98a468a99d2b30b688da411fd4a86edd76e994
Cinthia iza <cizac1@est.ups.edu.ec>
2020-06-27 11:58:06 -0500

    Verficacion

--- pruebaGIT/src/pruebaGIT/PruebaGIT.java
+++ pruebaGIT/src/pruebaGIT/PruebaGIT.java
@@ -3,7 +3,7 @@ package pruebaGIT;
 public class PruebaGIT {
 	private String nombre;
 	private String Apeliido;
-
+	private String cedula;
 	public PruebaGIT() {
 		// TODO Auto-generated constructor stub
 		
